7784d10e6a793da6c7039d611be9d4bf32d94161
Dayana M <dbmv23@gmail.com>
2021-05-10 18:45:29 -0500

    first commit local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'localstorage';
+
+  constructor() {
+
+    this.grabar_localstorage();
+    this.obtener_localstorage();
+  }
+
+  obtener_localstorage(){
+
+    let nombre = localStorage.getItem("nombre");
+    let persona = JSON.parse(localStorage.getItem("persona")) ;
+    console.log(nombre);
+    console.log(persona);
+  }
+  grabar_localstorage (){
+
+    let nombre: string = "Dayana";
+
+
+    let persona = {
+      nombre: "Lee",
+      edad: 41,
+      cords : {
+        lat:10,
+        lng: -10
+
+      }
+
+    }
+
+    localStorage.setItem("nombre", nombre);
+    localStorage.setItem("persona", JSON.stringify(persona) );
+  }
 }
